d774ddb144012f698af5bd3d94d2bc45f4dc8249
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-24 00:10:00 +0100

    fix 2

--- frontend/dev-portfolio/next.config.ts
+++ frontend/dev-portfolio/next.config.ts
@@ -1,21 +1,19 @@
 import type { NextConfig } from "next";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
-const API_BASE_HOST = process.env.NEXT_PUBLIC_API_BASE_HOST
-const API_BASE_PORT = process.env.NEXT_PUBLIC_API_BASE_PORT
+const API_BASE_HOST = process.env.NEXT_PUBLIC_API_BASE_HOST;
+const API_BASE_PORT = process.env.NEXT_PUBLIC_API_BASE_PORT;
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: `${API_BASE_HOST}`,
-        port: API_BASE_PORT || undefined,
-        pathname: '/media/**'
+        protocol: process.env.NODE_ENV === "development" ? "http" : "https",
+        hostname: API_BASE_HOST!,
+        port: API_BASE_PORT || undefined, // ✅ only set if exists
+        pathname: "/media/**",
       },
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
