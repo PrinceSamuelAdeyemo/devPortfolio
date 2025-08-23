f578b2d1d88c33afd78995d8a3b29512f0ac6511
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-23 08:47:54 +0100

    installed django unfold

--- backend/devPortfolioBackend/core/admin.py
+++ backend/devPortfolioBackend/core/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import (Expertise, Tool, ProgrammingLanguage, ExpertiseSkill, Project, Article)
+
 # Register your models here.
-admin.site.register(Expertise)
-admin.site.register(Tool)
-admin.site.register(ProgrammingLanguage)
-admin.site.register(ExpertiseSkill)
-admin.site.register(Project)
-admin.site.register(Article)
+@admin.register(Expertise)
+class ExpertiseAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Tool)
+class ToolAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(ProgrammingLanguage)
+class ProgrammingLanguageAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(ExpertiseSkill)
+class ExpertiseSkillAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Article)
+class ArticleAdminClass(ModelAdmin):
+    pass
