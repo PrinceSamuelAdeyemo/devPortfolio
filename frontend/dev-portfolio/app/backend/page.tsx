f56d57797af09c23fd0447a9d2f9b3d921c23d9f
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-26 13:36:45 +0100

    backend page responsiveness done

--- frontend/dev-portfolio/app/backend/page.tsx
+++ frontend/dev-portfolio/app/backend/page.tsx
@@ -56,8 +56,8 @@ export default async function Backend(){
                 {/* HERO SECTION */}
                 <div className="py-28 bg-gradient-to-r from-white via-personalpurple via-50% to-white to-95%">
                     <div className="flex flex-col items-center gap-4">
-                        <p className="text-5xl font-bold">Backend Development</p>
-                        <p className="text-base text-gray-400 text-center w-[30%]">Crafting robust, scalable, and secure server-side solutions that power modern applications. Explore my deep understanding of backend systems and architecture.</p>
+                        <p className="text-2xl lg:text-5xl font-bold">Backend Development</p>
+                        <p className="text-base text-gray-600 text-center w-[90%] md:w-[50%] lg:w-[30%]">Crafting robust, scalable, and secure server-side solutions that power modern applications. Explore my deep understanding of backend systems and architecture.</p>
                     </div>
                 </div>
 
@@ -65,8 +65,8 @@ export default async function Backend(){
                 <div className="flex flex-col gap-8 lg:gap-20 items-center">
                     {/* Skills */}
                     <div className="flex flex-col gap-6 w-4/5">
-                        <p className="font-semibold text-2xl text-center ">Core Backend Skills</p>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:gap-2 xl:gap-4">
+                        <p className="font-semibold text-xl lg:text-2xl text-center ">Core Backend Skills</p>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-2 xl:gap-4">
                             {
                                 myBackendSkills.map((skill: myExpertiseSkillsType) => (
                                     <Skills key={skill.id} {...skill} />
@@ -77,7 +77,7 @@ export default async function Backend(){
 
                     {/* featured frontend projects */}
                     <div className="flex flex-col gap-6 w-4/5">
-                        <p className="font-semibold text-2xl text-center ">Featured Backend Projects</p>
+                        <p className="font-semibold text-xl lg:text-2xl text-center ">Featured Backend Projects</p>
                         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                             {
                                 myProjects.map((featured_project, index) => (
@@ -89,7 +89,7 @@ export default async function Backend(){
 
                     {/* Articles/Writeups */}
                     <div className="flex flex-col gap-6 w-4/5">
-                        <p className="font-semibold text-2xl text-center ">Backend Architecture Spotlights</p>
+                        <p className="font-semibold text-xl lg:text-2xl text-center ">Backend Architecture Spotlights</p>
                         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                             {/* <FeaturedProject />
                             <FeaturedProject />
