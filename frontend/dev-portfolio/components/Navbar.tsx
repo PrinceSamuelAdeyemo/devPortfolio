457933c6eb71df3debc428c22bdaff41a58e7dcf
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-26 10:25:27 +0100

    minor fix on navbar indexing

--- frontend/dev-portfolio/components/Navbar.tsx
+++ frontend/dev-portfolio/components/Navbar.tsx
@@ -12,10 +12,10 @@ export default function Navbar() {
   const [toggleNavbar, setToggleNavbar] = useState<boolean>(false)
   
   return (
-    <nav className="flex p-4 gap-10 border-b-1 border-gray-200 w-full relative lg:fixed lg:z-50">
-      <h1 className="flex items-center gap-1 font-semibold z-10"><span><i><Image src={"/icons/logo.svg"} alt="" width={30} height={30} /></i></span>My Portfolio</h1>
+    <nav className="flex p-4 gap-10 border-b-1 border-gray-200 w-full relative lg:fixed lg:z-30">
+      <h1 className="flex items-center gap-1 font-semibold z-30"><span><i><Image src={"/icons/logo.svg"} alt="" width={30} height={30} /></i></span>My Portfolio</h1>
       <div className="flex">
-        <ul className={`${toggleNavbar===false ? 'hidden' : 'flex flex-col pt-16 pb-4 bg-personallightblue'} lg:flex lg:flex-row lg:py-0 lg:bg-inherit items-center w-full gap-10 lg:w-1/2 absolute top-0 left-0 lg:static`}>
+        <ul className={`${toggleNavbar===false ? 'hidden' : 'flex flex-col pt-16 pb-4 bg-personallightblue z-20'} lg:flex lg:flex-row lg:py-0 lg:bg-inherit items-center w-full gap-10 lg:w-1/2 absolute top-0 left-0 lg:static`}>
             <li><Link className={`${pathname=='/' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/"}>Home</Link></li>
             <li><Link className={`${pathname=='/frontend' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/frontend"}>Frontend</Link></li>
             <li><Link className={`${pathname=='/backend' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/backend"}>Backend</Link></li>
@@ -24,7 +24,7 @@ export default function Navbar() {
             <li><Link className={`${pathname=='/ai_ml' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/ai-ml"}>AI/ML</Link></li>
         </ul>
       </div>
-      <div onClick={() => setToggleNavbar(!toggleNavbar)} className="flex flex-col lg:hidden gap-1 z-10 ml-auto w-[2.3rem] h-6">
+      <div onClick={() => setToggleNavbar(!toggleNavbar)} className="flex flex-col lg:hidden gap-1 z-20 ml-auto w-[2.3rem] h-6">
         <div className={`${toggleNavbar===true ? 'rotate-45 translate-2 origin-center' : 'rotate-none' } w-full h-1 bg-gray-500 rounded`}></div>
         <div className={`${toggleNavbar===true ? 'hidden' : 'rotate-none w-full h-1 bg-gray-500 rounded' }`}></div>
         <div className={`${toggleNavbar===true ? '-rotate-48 translate-x-1.5 transition-all' : 'rotate-none' } w-full h-1 bg-gray-500 rounded`}></div>
