3e7e0d271b76eab5d49e0722620fc23036186c92
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-26 14:28:34 +0100

    minor text adjustment

--- frontend/dev-portfolio/components/Skills.tsx
+++ frontend/dev-portfolio/components/Skills.tsx
@@ -5,9 +5,9 @@ import { baseUrl } from "@/utils/api";
 export default function Skills(skill: myExpertiseSkillsType)  {
 
     return (
-        <div className="flex flex-col items-center p-6 gap-3 w-full rounded shadow-xs shadow-gray-600">
+        <div className="flex flex-col items-center py-6 px-2 lg:p-6 gap-3 w-full rounded shadow-xs shadow-gray-600">
             <Image className="rounded" src={`${baseUrl}${skill.icon}`} alt="Frontend icon" width={30} height={30} />
-            <p className="font-bold md:text-xl">{skill.name}</p>
+            <p className="font-bold text-center md:text-xl">{skill.name}</p>
             <p className="text-gray-500 text-base text-center">{skill.description}</p>
         </div>
     )
