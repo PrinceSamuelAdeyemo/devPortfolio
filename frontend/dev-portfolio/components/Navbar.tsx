08e5f319670da16a6de57b80496e4a1ea0d5b4dd
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-26 09:33:02 +0100

    done with the navbar responsiveness, updated gitignore

--- frontend/dev-portfolio/components/Navbar.tsx
+++ frontend/dev-portfolio/components/Navbar.tsx
@@ -4,30 +4,30 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
 
-  const toggleNavbar = useState<boolean>(false)
+  const [toggleNavbar, setToggleNavbar] = useState<boolean>(false)
   
   return (
     <nav className="flex p-4 gap-10 border-b-1 border-gray-200 w-full relative lg:fixed lg:z-50">
       <h1 className="flex items-center gap-1 font-semibold z-10"><span><i><Image src={"/icons/logo.svg"} alt="" width={30} height={30} /></i></span>My Portfolio</h1>
       <div className="flex">
-        <ul className="nav-links flex flex-col lg:flex-row items-center w-full pt-16 pb-4 bg-amber-100 gap-10 lg:w-1/2 absolute top-0 left-0 lg:static">
-            <li><Link className={`${pathname=='/' ? 'text-personalpurple' : 'text-black'}`} href={"/"}>Home</Link></li>
-            <li><Link className={`${pathname=='/frontend' ? 'text-personalpurple' : 'text-black'}`} href={"/frontend"}>Frontend</Link></li>
-            <li><Link className={`${pathname=='/backend' ? 'text-personalpurple' : 'text-black'}`} href={"/backend"}>Backend</Link></li>
-            <li><Link className={`${pathname=='/fullstack' ? 'text-personalpurple' : 'text-black'}`} href={"/fullstack"}>Fullstack</Link></li>
-            <li><Link className={`${pathname=='/blockchain' ? 'text-personalpurple' : 'text-black'}`} href={"/blockchain"}>Blockchain</Link></li>
-            <li><Link className={`${pathname=='/ai_ml' ? 'text-personalpurple' : 'text-black'}`} href={"/ai-ml"}>AI/ML</Link></li>
+        <ul className={`${toggleNavbar===false ? 'hidden' : 'flex flex-col pt-16 pb-4 bg-personallightblue'} lg:flex lg:flex-row lg:py-0 lg:bg-inherit items-center w-full gap-10 lg:w-1/2 absolute top-0 left-0 lg:static`}>
+            <li><Link className={`${pathname=='/' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/"}>Home</Link></li>
+            <li><Link className={`${pathname=='/frontend' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/frontend"}>Frontend</Link></li>
+            <li><Link className={`${pathname=='/backend' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/backend"}>Backend</Link></li>
+            <li><Link className={`${pathname=='/fullstack' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/fullstack"}>Fullstack</Link></li>
+            <li><Link className={`${pathname=='/blockchain' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/blockchain"}>Blockchain</Link></li>
+            <li><Link className={`${pathname=='/ai_ml' ? 'text-personalpurple font-bold' : 'text-black font-semibold lg:font-normal'}`} href={"/ai-ml"}>AI/ML</Link></li>
         </ul>
       </div>
-      <div onClick={toggleNavbar} className="flex flex-col gap-1 z-10 ml-auto w-[2.3rem] h-6">
-        <div className=`${toggleNavbar ? /../: } w-full h-1 bg-gray-500 rounded``></div>
-        <div className="w-full h-1 bg-gray-500 rounded"></div>
-        <div className="w-full h-1 bg-gray-500 rounded"></div>
+      <div onClick={() => setToggleNavbar(!toggleNavbar)} className="flex flex-col lg:hidden gap-1 z-10 ml-auto w-[2.3rem] h-6">
+        <div className={`${toggleNavbar===true ? 'rotate-45 translate-2 origin-center' : 'rotate-none' } w-full h-1 bg-gray-500 rounded`}></div>
+        <div className={`${toggleNavbar===true ? 'hidden' : 'rotate-none w-full h-1 bg-gray-500 rounded' }`}></div>
+        <div className={`${toggleNavbar===true ? '-rotate-48 translate-x-1.5 transition-all' : 'rotate-none' } w-full h-1 bg-gray-500 rounded`}></div>
       </div>
     </nav>
   );
