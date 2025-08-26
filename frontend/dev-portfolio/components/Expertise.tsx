1840ffb2488af38819df38dce4f3e00ace8d374b
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-26 10:30:11 +0100

    resized the expertise component

--- frontend/dev-portfolio/components/Expertise.tsx
+++ frontend/dev-portfolio/components/Expertise.tsx
@@ -7,7 +7,7 @@ export default function Expertise({name, description, icon, image}: myExpertiseT
     console.log(`${baseUrl}${icon}`)
 
     return (
-        <div className="flex flex-col p-4 gap-2 w-1/5 rounded shadow-xs shadow-gray-600">
+        <div className="flex flex-col p-4 gap-2 w-full lg:w-1/5 rounded shadow-xs shadow-gray-600">
             <Image className="rounded" src={icon ? `${BASE_URL}${icon}`: ''} alt="Frontend icon" width={30} height={30} />
             <p className="font-bold md:text-xl">{name} Development</p>
             <p className="text-gray-500 text-base">{description? description : 'Crafting intuitive and responsive user interfaces with modern frameworks like React and Next.js.'}</p>
